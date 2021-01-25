3fa9455329539f075e7fed7664ccc53a45139002
Bilyana Karcheva <bilyana.karcheva@gemseek.com>
2021-01-25 16:58:40 +0200

    Add new file

---
+++ 6._JaggedArray_Modification/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixRows = int.Parse(Console.ReadLine());
+
+            int[][] jaggedArray = new int[matrixRows][];
+
+            for (int row = 0; row < matrixRows; row++)
+            {
+                int[] col = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                jaggedArray[row] = col;
+            }
+
+            string[] command = Console.ReadLine().Split(' ').ToArray();
+
+            while (!command.Contains("END"))
+            {
+                int row = int.Parse(command[1]);
+                int col = int.Parse(command[2]);
+                int value = int.Parse(command[3]);
+
+                if (row < 0 || row >= jaggedArray.Length || col >= jaggedArray[row].Length || col < 0)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                else
+                {
+                    if (command.Contains("Add"))
+                    {
+                        jaggedArray[row][col] += value; 
+                    }
+                    else if (command.Contains("Subtract"))
+                    {
+                        jaggedArray[row][col] -= value;
+                    }
+                }
+
+                command = Console.ReadLine().Split(' ').ToArray();
+
+            }
+
+            for (int row = 0; row < matrixRows; row++)
+            {
+                Console.WriteLine(String.Join(' ',jaggedArray[row])); 
+            }
+        }
+    }
+}
